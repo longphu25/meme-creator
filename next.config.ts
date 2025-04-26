e8f02d0e3aafef0a8652c925e468d473c3642434
Long Phu <longphu257@gmail.com>
2025-04-26 12:14:34 +0700

    Fix Issue basePath

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export",  // <=== enables static exports
-  basePath: "/meme-creator", // <=== base path for static export
+  output: "export", // <=== enables static exports
+  basePath: process.env.PAGES_BASE_PATH, // <=== base path for static export
   reactStrictMode: true,
 };
 
