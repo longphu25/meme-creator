ac6f440a090959df1db6b63e8dd38755ea63f61b
Long Phu <longphu257@gmail.com>
2025-04-26 11:16:04 +0700

    Github - Page

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",  // <=== enables static exports
+  basePath: "/meme-creator", // <=== base path for static export
+  reactStrictMode: true,
 };
 
 export default nextConfig;
